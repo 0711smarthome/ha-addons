b20f2981a2f615bd60d26358abcd642cb3c6e8c3
andiHei <color.ui>
2025-10-09 12:54:02 +0200

    V4: host_network false

--- wlan_shelly_scanner/run.sh
+++ wlan_shelly_scanner/run.sh
@@ -2,13 +2,20 @@
 
 echo "Start-Skript wird ausgeführt..."
 
+# D-Bus ist eine zwingende Voraussetzung für NetworkManager
+echo "Starte D-Bus System-Dienst..."
+dbus-daemon --system
+
+# Gib D-Bus einen Moment Zeit zum Starten
+sleep 1
+
 # Starte den NetworkManager-Dienst im Hintergrund
 echo "Starte NetworkManager-Dienst..."
 NetworkManager &
 
-# Gib dem Dienst einen Moment zum Initialisieren
+# Gib dem NetworkManager einen Moment zum Initialisieren
 sleep 3
 
-echo "NetworkManager sollte jetzt laufen. Starte main.py..."
+echo "Dienste gestartet. Starte main.py..."
 # Führe das Python-Skript als Hauptprozess aus
 exec python3 /main.py
